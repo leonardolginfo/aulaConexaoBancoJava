4f27672bf9917726fa627476611ef4b74ef3a91d
Leonardo Gonçalves <leonardo@lginfo.com.br>
2021-11-15 06:22:23 -0300

    adicionando parte3 + connection.properties

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Módulo Conexão com BD no Java
 ## Curso do bootcamp TQI na DIO
+### Na parte 2 a conexão está recebendo os dados de acesso diretamente
+### Na parte 3 a conexão está receberá os dados vindos do connection.properties
--- src/main/java/Parte2/ConnectionJDBC.java
+++ src/main/java/Parte2/ConnectionJDBC.java
@@ -5,18 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionJDBC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        String passw = "784512";
-        String userBd="postgres";
-        String urlConnection = "jdbc:postgresql://localhost:5432/digital_dio_tqi/";
-        Connection conectadBd=null;
-try {
-    conectadBd = DriverManager.getConnection(urlConnection, userBd,passw);
-} catch (SQLException throwables) {
-    throwables.printStackTrace();
+        // 2 - Definir parâmetros para se conectar ao banco de dados MySQL.
+        String driver = "postgresql";
+        String dataBaseAddress = "localhost";
+        String dataBaseName = "digital_dio_tqi";
+        String user = "rootnew";
+        String password = "123456";
+
+        String sbd = "jdbc:" + driver + "://" + dataBaseAddress + "/" + dataBaseName;
+
+        String connectionUrl; //sb.toString() == "jdbc:postgresql://localhost/digital_dio_tqi"
+        connectionUrl = sbd;
+
+
+        try (Connection conn = DriverManager.getConnection(connectionUrl, user, password)) {
+            System.out.println("SUCESSO ao se conectar ao banco Postgres!");
+        } catch (SQLException e) {
+            System.out.println("FALHA ao se conectar ao banco Postgres!");
+            e.printStackTrace();
+        }
+    }
 }
-    }
-    }
 
 
